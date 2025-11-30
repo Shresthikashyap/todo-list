579a0a1fd2f39801e76a4030af81976d411a4256
Shresthi Kashyap <sk2929542@gmail.com>
2025-11-30 14:09:14 +0530

    Initial commit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -23,9 +23,9 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-  connectDB();
+app.listen(port, async() => {
+  console.log(`Server is running on port ${port}`);
+  await connectDB();
 });
